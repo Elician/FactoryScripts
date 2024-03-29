d39930e51dfb29a09b2e5e5968c4ce241272f171
Dmitry Sidorov <61616100+JustyDev@users.noreply.github.com>
2024-03-29 17:07:06 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -12,19 +12,25 @@ tar_files="./scripts.tar"
 tar -cf ${tar_files} scripts
 [ ! -f ${tar_files} ] && echo "Ошибка создания архива" && exit
 
-base64tar=$(base64 ${tar_files})
+base64tar=$(base64 ${tar_files} -w 0)
+#echo "${base64tar}" >> base64.txt
+func_write_temp_json() {
+  echo "{
+        \"mod_pack_id\": ${mod_pack_id},
+        \"data\": \"${base64tar}\",
+        \"syncing\": {
+            \"servers_prod\": ${servers_prod},
+            \"servers_dev\": ${servers_dev},
+            \"client_prod\": ${client_prod},
+            \"client_dev\": ${client_dev}
+        }
+      }" >> temp.json
+}
+func_write_temp_json
+
 curl -s \
     -H "Authorization: Bearer ${bearer}" \
     -H "Content-Type: application/json" \
     -X POST \
-    -d '{
-        "mod_pack_id": ${mod_pack_id},
-        "data": ${base64tar},
-        "syncing": {
-            "servers_prod": ${servers_prod},
-            "servers_dev": ${servers_dev},
-            "client_prod": ${client_prod},
-            "client_dev": ${client_dev}
-        }
-      }' \
+    -d @temp.json\
     https://api.elician.ru/system.syncScripts
